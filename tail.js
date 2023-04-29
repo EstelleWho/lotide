89f35d0c59ede906df1788fb4377d1d618bf15b2
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-04-29 17:50:23 -0400

    refait + a propos

--- tail.js
+++ tail.js
@@ -1,3 +1,6 @@
+//A PROPOS: ___.tail([ ]) is a method that returns all elements of an array except the first one [0].
+// meow.tail([1, 2, 3, 4]); output: [2, 3, 4]
+
 //Create a function 'tail' which returns the "tail" of an array: everything except for the first item (head) of the provided array.
 
 
@@ -11,13 +14,36 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(array) {
   return array.slice(1);
-}
+};
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
+console.log(tail(words));
 
+/* REFAIT */
+/*
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+*/
 
-
+//with .push() method
+/*
+const tail = function(array) {
+  if (array.length <= 1) {
+    return [];
+  }
+  const tailArray = [];
+  for (let i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+};
+*/
