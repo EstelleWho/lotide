1b57234b840616b3e15021e65b4d617f0bec9ebb
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-05-12 19:40:57 -0400

    added require

---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+const middle = require("../middle");
+
+
+
+
+
+// Take an array and return the middle-most elements of the array.
+// The array must have at least 3 elements. If less than 3 elements, return an empty array,
+// Odd number of elements, return single middle element.
+// Even number of elements, return two middle elements.
