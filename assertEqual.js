e46605cb55dcd37b51ed5ddd23b69315cf287567
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-05-12 16:49:02 -0400

    added export

--- assertEqual.js
+++ assertEqual.js
@@ -13,19 +13,6 @@ module.exports = assertEqual;
 
 // A PROPOS: ES6 Template Literals/Template Strings: this is used to concatenate(ie add) strings together and make it look fancier, simpler, and faster! Instead of console.log('Hello, ' + name + '!'), you would write console.log(`'Hello, ${name}!`), you `${INTERPOLATE(ie inject)} the variable inside back-ticks`.
 
-/*
-****** En premier: *******
-
-#1
 // FUNCTION IMPLEMENTATION
 //template literals, template strings
 //which allow you to insert variables / JS into strings by using back-ticks `` and the ${} symbol.
-
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-};
-*/
