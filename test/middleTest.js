214a854de3f120f6b21f61e1667357a1b7de3218
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-05-13 18:16:49 -0400

    M&C-ed

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,16 @@
-const middle = require("../middle");
+const middle   = require('../middle');
+const assert = require('chai').assert;
 
 
+describe("#middle", () => {
 
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
 
 
 // Take an array and return the middle-most elements of the array.
